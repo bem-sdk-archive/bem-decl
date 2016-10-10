c1372a8e696576e72b27061ef68b7bf7bc092aeb
Anton Krichevskii <mrskadi0@gmail.com>
2016-10-10 18:54:55 +0300

    Add sugar convertation and tests + relative stringify test

--- test/convert/v1.test.js
+++ test/convert/v1.test.js
@@ -8,26 +8,128 @@ test('must return empty decl', t => {
     t.deepEqual(convert([], { format: 'v1' }), []);
 });
 
-test('must transform deps to bemdecl', t => {
+test('must split elems of one block', t => {
     const input = [
         { entity: { block: 'block1' }, tech: null },
-        { entity: { block: 'block1', modName: 'mod1', modVal: true }, tech: null },
-        { entity: { block: 'block1', modName: 'mod1', modVal: 'val1' }, tech: null },
         { entity: { block: 'block1', elem: 'elem1' }, tech: null },
-        { entity: { block: 'block1', elem: 'elem1', modName: 'mod2', modVal: true }, tech: null },
-        { entity: { block: 'block1', elem: 'elem1', modName: 'mod2', modVal: 'val2' }, tech: null }
+        { entity: { block: 'block1', elem: 'elem2' }, tech: null }
     ];
     const output = [
-        { name: 'block1' },
-        { name: 'block1', mods: [{ name: 'mod1', vals: [{ name: true }] }] },
-        { name: 'block1', mods: [{ name: 'mod1', vals: [{ name: 'val1' }] }] },
-        { name: 'block1', elems: [{ name: 'elem1' }] },
-        { name: 'block1', elems: [{ name: 'elem1', mods: [{ name: 'mod2', vals: [{ name: true }] }] }] },
-        { name: 'block1', elems: [{ name: 'elem1', mods: [{ name: 'mod2', vals: [{ name: 'val2' }] }] }] }
+        { name: 'block1', elems: [{ name: 'elem1' }, { name: 'elem2' }] }
     ];
 
     t.deepEqual(
         convert(input, { format: 'v1' }),
         output
+    );
+});
+
+test('must split mods of one block', t => {
+    const input = [
+        { entity: { block: 'block1' }, tech: null },
+        { entity: { block: 'block1', modName: 'mod1', modVal: 'val1' }, tech: null },
+        { entity: { block: 'block1', modName: 'mod2', modVal: 'val2' }, tech: null }
+    ];
+    const output = [
+        {
+            name: 'block1',
+            mods: [
+                { name: 'mod1', vals: [{ name: 'val1' }] },
+                { name: 'mod2', vals: [{ name: 'val2' }] }
+            ]
+        }
+    ];
+
+    t.deepEqual(
+        convert(input, { format: 'v1' }),
+        output
+    );
+});
+
+test('must split vals of mods block', t => {
+    const input = [
+        { entity: { block: 'block1' }, tech: null },
+        { entity: { block: 'block1', modName: 'mod1', modVal: true }, tech: null },
+        { entity: { block: 'block1', modName: 'mod1', modVal: 'val1' }, tech: null }
+    ];
+    const output = [
+        {
+            name: 'block1',
+            mods: [
+                { name: 'mod1', vals: [{ name: true }, { name: 'val1' }] }
+            ]
+        }
+    ];
+
+    t.deepEqual(
+        convert(input, { format: 'v1' }),
+        output
+    );
+});
+
+test('must split elem mods of block', t => {
+    const input = [
+        { entity: { block: 'block1' }, tech: null },
+        { entity: { block: 'block1', elem: 'elem1', modName: 'mod1', modVal: 'val1' }, tech: null },
+        { entity: { block: 'block1', elem: 'elem1', modName: 'mod2', modVal: 'val2' }, tech: null }
+    ];
+    const output = [
+        {
+            name: 'block1',
+            elems: [
+                {
+                    name: 'elem1',
+                    mods: [
+                        { name: 'mod1', vals: [{ name: 'val1' }] }, { name: 'mod2', vals: [{ name: 'val2' }] }
+                    ]
+                }
+            ]
+        }
+    ];
+
+    t.deepEqual(
+        convert(input, { format: 'v1' }),
+        output
+    );
+});
+
+test('must split vals of elem mods', t => {
+    const input = [
+        { entity: { block: 'block1' }, tech: null },
+        { entity: { block: 'block1', elem: 'elem1', modName: 'mod1', modVal: 'val1' }, tech: null },
+        { entity: { block: 'block1', elem: 'elem1', modName: 'mod1', modVal: 'val2' }, tech: null }
+    ];
+    const output = [
+        {
+            name: 'block1',
+            elems: [
+                {
+                    name: 'elem1',
+                    mods: [
+                        { name: 'mod1', vals: [{ name: 'val1' }, { name: 'val2' }] }
+                    ]
+                }
+            ]
+        }
+    ];
+
+    t.deepEqual(
+        convert(input, { format: 'v1' }),
+        output
+    );
+});
+
+test('should create full entity with mods', t => {
+    t.deepEqual(
+        convert({ entity: { block: 'block1', modName: 'mod1', modVal: 'val1' } }, { format: 'v1' }),
+        [{
+            name: 'block1',
+            mods: [{
+                name: 'mod1',
+                vals: [{
+                    name: 'val1'
+                }]
+            }]
+        }]
     );
 });
---
+++ test/stringify/v1.test.js
@@ -0,0 +1,25 @@
+const test = require('ava');
+
+const stringify = require('../../lib/stringify');
+
+test('should stringify enb declaration', t => {
+    const obj = [{
+        name: 'block',
+        elems: [{
+            name: 'elem',
+            mods: [{
+                name: 'mod',
+                vals: [{
+                    name: 'val'
+                }]
+            }]
+        }]
+    }];
+    t.deepEqual(
+        stringify(
+            { entity: { block: 'block', elem: 'elem', modName: 'mod', modVal: 'val' }, tech: null },
+            { format: 'v1' }
+        ),
+        'exports.blocks = ' + JSON.stringify(obj, null, 4) + ';\n'
+    );
+});
