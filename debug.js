c3322923afe77abbe8e81817dd5554668b927aa6
Konstantin Gladkikh <birhoff@gmail.com>
2016-12-14 13:53:24 +0300

    asd

--- debug.js
+++ debug.js
@@ -3,11 +3,11 @@ const format = require('./lib/format');
 const input = [
     {entity: {block: 'block1'}, tech: null},
     {entity: {block: 'block1', elem: 'elem1'}, tech: null},
-    {entity: {block: 'block1', elem: 'elem2'}, tech: null},
+    {entity: {block: 'block1', elem: 'elem1', modName: 'm1', modVal: 'v1'}, tech: null},
 
-    {entity: {block: 'block2'}, tech: null},
-    {entity: {block: 'block2', elem: 'elem1'}, tech: null},
-    {entity: {block: 'block2', elem: 'elem2'}, tech: null},
+    // {entity: {block: 'block1', modName: 'm1', modVal: 'v1'}, tech: null},
+
+    {entity: {block: 'block1', elem: 'elem1', modName: 'm2', modVal: 'v2'}, tech: null},
 ];
 
 const tree = format(input, {format: 'v2'});
