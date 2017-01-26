d7bc1d66063e26e4d7b52cb7b613b56b2d15a620
Anton Krichevskii <mrskadi0@gmail.com>
2017-01-26 21:12:22 +0300

    fixup! fixup! Convert renamed to format

--- test/format/enb.test.js
+++ test/format/enb.test.js
@@ -2,19 +2,19 @@
 
 const test = require('ava');
 
-const formatter = require('../../lib/format');
+const format = require('../../lib/format');
 
 test('should throw exception if no format given', t => {
-    t.throws(() => formatter({ entity: { block: 'block' }, tech: null }), 'You must declare target format');
+    t.throws(() => format({ entity: { block: 'block' }, tech: null }), 'You must declare target format');
 });
 
 test('should format to enb format', t => {
-    t.deepEqual(formatter({ entity: { block: 'block' }, tech: null }, { format: 'enb' }), [{ block: 'block' }]);
+    t.deepEqual(format({ entity: { block: 'block' }, tech: null }, { format: 'enb' }), [{ block: 'block' }]);
 });
 
 test('should coformatnvert with elem', t => {
     t.deepEqual(
-        formatter([
+        format([
             { entity: { block: 'block' }, tech: null },
             { entity: { block: 'block', elem: 'elem' }, tech: null }
         ], { format: 'enb' }),
@@ -27,7 +27,7 @@ test('should coformatnvert with elem', t => {
 
 test('should format with mod', t => {
     t.deepEqual(
-        formatter([
+        format([
             { entity: { block: 'block', modName: 'mod', modVal: 'val' }, tech: null }
         ], { format: 'enb' }),
         [{ block: 'block', mod: 'mod', val: 'val' }]
