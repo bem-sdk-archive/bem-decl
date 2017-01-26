33c05136f7bddeee4257faa3bb90b65772c9d5e2
Anton Krichevskii <mrskadi0@gmail.com>
2017-01-26 22:11:39 +0300

    Update stringify method

--- test/stringify/enb.test.js
+++ test/stringify/enb.test.js
@@ -1,18 +1,51 @@
 const test = require('ava');
+const BemCell = require('@bem/cell');
+const BemEntityName = require('@bem/entity-name');
+const JSON5 = require('json5');
 
 const stringify = require('../../lib/stringify');
 
-test('should throws error if no format given', t => {
-    t.throws(() => stringify({ entity: { block: 'block' }, tech: null }), 'You must declare target format');
+const obj = {
+    format: 'enb',
+    decl: [{ block: 'block', elem: 'elem', mod: 'mod', val: 'val' }]
+};
+const cell = new BemCell({
+    entity: new BemEntityName({ block: 'block', elem: 'elem', modName: 'mod', modVal: 'val' }),
+    tech: null
 });
 
-test('should stringify enb declaration', t => {
-    const obj = [{ block: 'block', elem: 'elem', mod: 'mod', val: 'val' }];
-    t.deepEqual(
-        stringify(
-            { entity: { block: 'block', elem: 'elem', modName: 'mod', modVal: 'val' }, tech: null },
-            { format: 'enb' }
-        ),
-        'exports.deps = ' + JSON.stringify(obj, null, 4) + ';\n'
+test('should throws error if no format given', t => {
+    t.throws(() => stringify(
+        new BemCell({ entity: new BemEntityName({ block: 'block' }), tech: null })),
+        'You must declare target format'
     );
 });
+
+test('should stringify enb declaration with commonJS', t => {
+    t.deepEqual(
+        stringify(cell, { format: 'enb', exportType: 'cjs' }),
+        `module.exports = ${JSON.stringify(obj, null, 4)};\n`
+    );
+});
+
+test('should stringify enb declaration with es6', t => {
+    t.deepEqual(
+        stringify(cell, { format: 'enb', exportType: 'es6' }),
+        `export default ${JSON.stringify(obj, null, 4)};\n`
+    );
+});
+
+test('should stringify enb declaration with JSON', t => {
+    t.deepEqual(
+        stringify(cell, { format: 'enb', exportType: 'json' }),
+        JSON.stringify(obj, null, 4)
+    );
+});
+
+test('should stringify enb declaration with JSON5', t => {
+    t.deepEqual(
+        stringify(cell, { format: 'enb', exportType: 'json5' }),
+        JSON5.stringify(obj, null, 4)
+    );
+});
+
