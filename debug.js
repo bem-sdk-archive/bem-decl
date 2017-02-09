f7ed39cf3bbdf6fc880b3445485b6382c442713a
Anton Krichevskii <mrskadi0@gmail.com>
2017-02-09 15:53:45 +0300

    sdsdsds

--- debug.js
+++ debug.js
@@ -1,17 +1,18 @@
 const aet = require('./lib/format/aet');
 const JSON5 = require('json5');
+const BemCell = require('@bem/cell');
 
 const pretty = obj => JSON5.stringify(obj, null, 2);
 
 const input = [
-    { entity: { block: 'block1' }, tech: null },
-    { entity: { block: 'block2' }, tech: null },
-    { entity: { block: 'block1', elem: 'elem1' }, tech: null },
-    { entity: { block: 'block1', elem: 'elem1', modName: 'm1', modVal: 'v1' }, tech: null },
+    BemCell.create({ entity: { block: 'block1' }, tech: null }),
+    BemCell.create({ entity: { block: 'block2' }, tech: null }),
+    BemCell.create({ entity: { block: 'block1', elem: 'elem1' }, tech: null }),
+    BemCell.create({ entity: { block: 'block1', elem: 'elem1', modName: 'm1', modVal: 'v1' }, tech: null }),
 
-    { entity: { block: 'block1', elem: 'elem1', modName: 'm1', modVal: 'v2' }, tech: null },
+    BemCell.create({ entity: { block: 'block1', elem: 'elem1', modName: 'm1', modVal: 'v2' }, tech: null }),
 
-    { entity: { block: 'block1', elem: 'elem1', modName: 'm2', modVal: 'v2' }, tech: null },
+    BemCell.create({ entity: { block: 'block1', elem: 'elem1', modName: 'm2', modVal: 'v2' }, tech: null }),
 ];
 
 const tree = aet(input);
