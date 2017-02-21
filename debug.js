3bc5dc3380acfa8c55b9dbb9500c881b668fb74c
Anton Krichevskii <mrskadi0@gmail.com>
2017-02-21 16:43:53 +0300

    fix

--- debug.js
+++ debug.js
@@ -7,7 +7,7 @@ const pretty = obj => JSON5.stringify(obj, null, 2);
 const input = [
     BemCell.create({ entity: { block: 'block1' }, tech: null }),
     // BemCell.create({ entity: { block: 'block2' }, tech: null }),
-    // BemCell.create({ entity: { block: 'block1', elem: 'elem1' }, tech: null }),
+    BemCell.create({ entity: { block: 'block1', elem: 'elem1' }, tech: null }),
     BemCell.create({ entity: { block: 'block1', elem: 'elem1', modName: 'm1', modVal: 'v1' }, tech: null }),
 
     BemCell.create({ entity: { block: 'block1', elem: 'elem1', modName: 'm1', modVal: 'v2' }, tech: null }),
