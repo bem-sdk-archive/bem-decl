8480b7ef6c02737363fb914832452919b26780bf
Anton Krichevskii <mrskadi0@gmail.com>
2017-02-22 17:25:30 +0300

    sdsdsd

--- test/stringify/common.test.js
+++ test/stringify/common.test.js
@@ -1,7 +1,18 @@
 const test = require('ava');
 
+const BemCell = require('@bem/cell');
 const stringify = require('../../lib/stringify');
 
+const cell = BemCell.create({ block: 'block' });
+
 test('should throws error if no format given', t => {
-    t.throws(() => stringify({ entity: { block: 'block' }, tech: null }), 'You must declare target format');
+    t.throws(() => stringify(cell), 'You must declare target format');
+});
+
+test('should throws error if unsupported format given', t => {
+    t.throws(() => stringify(cell, { format: 'unsupported' }), 'Specified format isn\'t supported');
+});
+
+test('should throws error if unsupported exportType given', t => {
+    t.throws(() => stringify(cell, { format: 'enb', exportType: 'unsupported' }), 'Specified export type isn\'t supported');
 });
